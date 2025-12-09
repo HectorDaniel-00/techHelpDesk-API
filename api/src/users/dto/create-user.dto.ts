35b079cdb93049e673cbb3b41c8a5265b22e17c3
HectorDaniel-00 <danielvargas.nike@gmail.com>
2025-12-09 12:15:22 -0500

    feat: adding the user module with its validations and creating its interceptor and filters

---
+++ api/src/users/dto/create-user.dto.ts
@@ -0,0 +1,24 @@
+import {
+  IsEmail,
+  IsEnum,
+  IsNotEmpty,
+  IsString,
+  Length,
+  MinLength,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  @IsString()
+  @Length(5, 100)
+  name!: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  email!: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(8)
+  password!: string;
+}
---
+++ api/src/users/users.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from './entities/user.entity';
+import { UsersRepository } from './entities/user.repository';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([UserEntity])],
+  controllers: [UsersController],
+  providers: [UsersService, UsersRepository],
+  exports: [UsersRepository],
+})
+export class UsersModule {}
